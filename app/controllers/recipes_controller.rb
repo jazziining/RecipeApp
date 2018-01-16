fd2ddb3754af914aff17d2a1d48a7115114d5e0d
Jasmine <93n.jas@gmail.com>
2018-01-16 14:09:07 -0500

    create cuisine views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,21 @@
 class RecipesController < ApplicationController
 
   def index
-
+    @get_term = params[:search]
+      if @get_term == nil
+        @recipes == []
+      elsif @get_term.empty?
+        @recipes == []
+      else
+        @recipes = Recipe.for(@get_term)
+      end
   end
 
   def show
-
+    @get_id = params [:id]
+    @recipe = Recipe.get_recipe(get_id)
+    if current_user
+      @review = @recipe.reviews.build
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   resources :cuisines, only: [:index, :show]
   resources :recipes, only: [:index, :show]
+
+
 end
